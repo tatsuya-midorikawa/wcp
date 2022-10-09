9aba438b6c43568f9e6717aefb0a281692b7ae1c
midoliy <midoliy@outlook.jp>
2022-10-09 10:02:06 +0900

    wip

--- src/wcp.console/Program.fs
+++ src/wcp.console/Program.fs
@@ -1,2 +1,13 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿type FooCommand (args: string[]) =
+  inherit Wcp.Command(args)
+
+let cmd = FooCommand([| "proto://test%5Cfoo%5Cbar" |])
+cmd.ctx |> printfn "%A"
+
+let s1 = "foo"
+let s2 = "foo:a"
+
+s1.IndexOf(":") |> printfn "s1: %d"
+s2.IndexOf(":") |> printfn "s2: %d"
+
+s2.Substring(0, s2.IndexOf(":")) |> printfn "s2 substring: %s"
--- src/wcp/Command.fs
+++ src/wcp/Command.fs
@@ -1,17 +1,32 @@
 ﻿module Wcp
 
 open System.Linq
+open System.Web
+open System.Text
+
+type Context = { cmd: string; param: string }
 
 [<AbstractClass>]
 type Command (args: string[], ?is'fsx: bool) =
-  let raw = args
-  let context =
-    match is'fsx with
-    | Some is'fsx -> 
-      if is'fsx
-      then if 1 < args.Length then args[1] else ""
-      else ""
-    | None -> args.FirstOrDefault()
+  let args' = args
+  let raw' =
+    let parameter =
+      match is'fsx with
+      | Some is'fsx -> 
+        if is'fsx
+        then if 1 < args.Length then args[1] else ""
+        else ""
+      | None -> args.FirstOrDefault()
+    HttpUtility.UrlDecode(parameter, UTF8Encoding(false))
+  let ctx' = 
+    let idx = raw'.IndexOf ":"
+    let cmd = if 0 <= idx then raw'.Substring(0, idx) else ""
+    let param = if 0 <= idx then raw'.Substring(idx + 1) else ""
+    { cmd = cmd; param = param }
+
+  member __.args = args'
+  member __.raw = raw'
+  member __.ctx = ctx'
 
 let run<'T> (args: string[]) =
   0
